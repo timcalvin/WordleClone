54874813d81df989d64e2f052d579cda9d2b9302
Tim Bryant <timcalvin@gmail.com>
2024-06-14 09:25:34 -0400

    Initial Commit

---
+++ WordleClone/WordleCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  WordleCloneApp.swift
+//  WordleClone
+//
+//  Created by Tim Bryant on 6/14/24.
+//
+
+import SwiftUI
+
+@main
+struct WordleCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
